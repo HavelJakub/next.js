3d1a4e21300b114561ef9cb52c7ab577f24ac803
VošplouchTornádoCZgamingLP <102223160+HavelJakub@users.noreply.github.com>
2023-03-15 09:25:44 +0100

    welcomen

---
+++ maturitanext/components/welcome.js
@@ -0,0 +1,20 @@
+import styles from './welcomen.module.css';
+
+const MainComponent = () => {
+  return (
+    <main className={styles.main}>
+      <div className={styles.content}>
+        <h1>ShoeStepStore</h1>
+        <p>Kola duní a za oknem uhání, krajina lesů vod a strání</p><br />
+        <div className={styles.buttonz}>
+          <a href="#nejnej">Novinky</a>
+        </div>
+        <div className={styles.buttonz}>
+          <a href="#">SNEAKERS</a>         
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default MainComponent;
